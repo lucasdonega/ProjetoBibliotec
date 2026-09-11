3f0e894770eac7bd3d48a596782ec4f3d6627392
LUCAS DE OLIVEIRA DONEGA <54584111880@info.local>
2026-09-11 10:44:53 -0300

    :bug: fix: Correção de código pasta home

---
+++ home/script.js
@@ -0,0 +1,27 @@
+let nome = "Lucas";
+let idade = "16"
+
+//CONCATENAÇÃO
+console.log("Nome: " + nome + ", idade: " + idade);
+
+// INTEPOLAÇÃO 
+console.log(`Nome: ${nome}, idade: ${idade}`)
+
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+let soma = "5" + 3 
+let sub = "5" - 3 
+
+console.log(soma)
+console.log(sub)
+
+console.log(typeof soma)
+console.log(typeof sub)
+
+let numero = "42"
+
+let convertido = Number(numero)
+
+console.log(typeof convertido)
